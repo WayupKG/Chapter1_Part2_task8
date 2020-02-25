af7e012b095f9762e0be5cedc0dd64a6f3f6c719
WayupKG <51049379+WayupKG@users.noreply.github.com>
2020-02-25 12:19:38 +0600

    Task8

---
+++ task8.py
@@ -0,0 +1,10 @@
+def two_func():
+    a = int(input())
+    b = int(input())
+    if a < b:
+        for i in range(a, b + 1):
+            print(i)
+    else:
+        for i in range(a, b - 1, -1):
+            print(i)
+two_func()
